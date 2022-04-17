6fbab9096887fbb39bf30a91f638b76240a281d1
Marlon Fabian Garcia Mendoza <marlon.garcia@systemico.co>
2022-04-17 16:16:38 -0500

    Implementacion del home y los componentes principales

---
+++ componentes/encabezado.php
@@ -0,0 +1,8 @@
+<nav class="navbar sticky-top navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="home.html">School Project</a>
+        <a href="/views/admin/userList.html">
+            <button type="button" class="btn btn-light">Ingresar</button>
+        </a>
+    </div>
+</nav>
